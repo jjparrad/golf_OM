1220997fcc70512f1be466a1c04b23308acca4c1
Bonetti Timothee <timothee.bonetti@etu.umontpellier.fr>
2025-04-17 13:06:14 +0200

    skybox (presque)

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@ GLFWwindow* window;
 #include "common/surface.hpp"
 #include "common/material.hpp"
 #include "common/light.hpp"
+#include "common/util.hpp"
 
 
 Mesh loadModel(std::string filename);
@@ -106,6 +107,10 @@ void drawObject(Mesh mesh) {
     );
 
 }
+/*************************************************************************** */
+
+
+
 
 
 /*******************************************************************************/
@@ -144,6 +149,53 @@ unsigned int loadTexture(const char* path) {
         return 0;
     }
 }
+
+unsigned int loadCubemap(std::vector<std::string> faces){
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        stbi_set_flip_vertically_on_load(false); // Ne pas retourner les textures de la skybox
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            GLenum format = GL_RGB;
+            if (nrChannels == 1)
+                format = GL_RED;
+            else if (nrChannels == 3)
+                format = GL_RGB;
+            else if (nrChannels == 4)
+                format = GL_RGBA;
+
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                         0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
+
+
+
+
+/*******************************************************************************/ 
 
 int main( void )
 {
@@ -205,6 +257,8 @@ int main( void )
     glBindVertexArray(VertexArrayID);
 
     // Create and compile our GLSL program from the shaders
+    GLuint programSky = LoadShaders( "../src/shaders/sky_vertex_shader.glsl", "../src/shaders/sky_fragment_shader.glsl" );
+
     GLuint programID = LoadShaders( "../src/shaders/vertex_shader.glsl", "../src/shaders/fragment_shader.glsl" );
 
     GLuint programBallID = LoadShaders( "../src/shaders/ball_vertex_shader.glsl", "../src/shaders/ball_fragment_shader.glsl" );
@@ -225,9 +279,33 @@ int main( void )
 
     GLuint texture_earth = loadTexture(TEXTURE_EARTH_PATH);
 
-    
+    std::vector<std::string> faces{
+        "../assets/cubemap_sky/right.png",   
+        "../assets/cubemap_sky/left.png",
+        
+            
+        "../assets/cubemap_sky/up.png",      
+        "../assets/cubemap_sky/down.png",
+        
+        "../assets/cubemap_sky/back.png",   
+        "../assets/cubemap_sky/front.png",   
+        
+          
+    };
+
+    GLuint textureSky = loadCubemap(faces);
+
+    GLuint skyboxVAO, skyboxVBO;
 
 
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBindVertexArray(0);
 
 
     GLuint vertexbuffer;
@@ -412,6 +490,28 @@ do {
     }
 
 
+    //====== sky shader =============
+    glDepthFunc(GL_LEQUAL);
+    glDepthMask(GL_FALSE);
+
+    glUseProgram(programSky);
+
+    glm::mat4 viewNoTranslation = glm::mat4(glm::mat3(view));
+    glUniformMatrix4fv(glGetUniformLocation(programSky, "view"), 1, GL_FALSE, &viewNoTranslation[0][0]);
+    glUniformMatrix4fv(glGetUniformLocation(programSky, "projection"), 1, GL_FALSE, &projection[0][0]);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureSky);
+    glUniform1i(glGetUniformLocation(programSky, "skybox"), 0);
+
+    glBindVertexArray(skyboxVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LESS);
+
+
 
     // Swap buffers
     glfwSwapBuffers(window);
@@ -455,6 +555,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+
 void setScene() {
     GameObject* surface = generateSurface(heightmapHeight, heightmapWidth, heightmapNrChannels,heightmapData);
     gameObjects.push_back(surface);
@@ -489,5 +590,5 @@ void setScene() {
     //sphere2->applytransform();
     gameObjects.push_back(sphere2);
 
-
+    //setupSkybox();
 }
